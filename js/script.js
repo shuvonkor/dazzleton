806677ef6d86bedb3e2e9a611064ec66a74b0087
Shuvonkor Pramanik <shuvonkorpramanik25@gmail.com>
2023-02-24 02:37:21 +0600

    final markup for this day

--- js/script.js
+++ js/script.js
@@ -15,4 +15,9 @@ function scrollFunction() {
 }
 
 //scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
+myButton.addEventListener("click", topFunction);
